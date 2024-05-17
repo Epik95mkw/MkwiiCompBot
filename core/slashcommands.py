6eac68cc72399a47142e9a76aa4c2878e0b4c1ff
Epik95mkw <epik955@gmail.com>
2024-05-17 10:42:07 -0400

    Logic for sending current submissions message

--- core/slashcommands.py
+++ core/slashcommands.py
@@ -58,18 +58,39 @@ class SlashCommands(commands.Cog):
     @slash_command()
     @guild_only()
     @default_permissions()
-    async def set_submission_files_channel(self, interaction: discord.Interaction):
-        """ Use this in channel where submissions should be sent. """
+    async def set_host_channel(self, interaction: discord.Interaction):
+        """ Use this in the channel where submissions should be sent. """
         self.bot.config.submission_channel_id = interaction.channel.id
         self.bot.update_config()
         await respond(interaction, 'Submission channel set to this channel')
 
+
     @slash_command()
     @guild_only()
     @default_permissions()
-    async def set_submission_list_channel(self, interaction: discord.Interaction):
-        """ Use this in channel where submissions should be sent. """
-        self.bot.config.submission_channel_id = interaction.channel.id
+    async def set_submissions_message_channel(self, interaction: discord.Interaction):
+        """ Use this in the channel where the current submissions message should be sent. """
+        channel_id = self.bot.config.submissions_message.channel_id
+        message_id = self.bot.config.submissions_message.message_id
+
+        if channel_id == interaction.channel.id:
+            await respond(interaction, 'Submission message channel is already set to this channel')
+            return
+
+        if channel_id is not None and message_id is not None:
+            old_channel = interaction.guild.get_channel(channel_id)
+            if old_channel is not None:
+                old_message = await old_channel.fetch_message(message_id)
+                if old_message is not None:
+                    await old_message.delete()
+
+        new_message = await interaction.channel.send(
+            '__**Current Submissions:**__\n' +
+            '\n'.join(f'{i + 1}. {username}' for i, username in enumerate(self.bot.config.task.submissions.keys()))
+        )
+
+        self.bot.config.submissions_message.channel_id = interaction.channel.id
+        self.bot.config.submissions_message.message_id = new_message.id
         self.bot.update_config()
         await respond(interaction, 'Submission channel set to this channel')
 
