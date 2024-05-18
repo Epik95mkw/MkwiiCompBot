f91e0a425306935715b0535b8f9f0d995d4466bf
Epik95mkw <epik955@gmail.com>
2024-05-17 22:33:17 -0400

    Remove /server_settings (might return later)

--- core/slashcommands.py
+++ core/slashcommands.py
@@ -17,37 +17,6 @@ class SlashCommands(commands.Cog):
     @slash_command()
     @guild_only()
     @default_permissions()
-    async def server_settings(
-            self,
-            interaction: discord.Interaction,
-            host_channel_id: int = None,
-            submission_list_channel_id: int = None
-    ):
-        """ Update one or more server settings directly. """
-        if self.bot.config.guild_id != interaction.guild.id:
-            await respond(interaction, 'Server has never been configured. Run /setup before using other commands.')
-            return
-
-        response_parts = []
-
-        if host_channel_id is not None:
-            self.bot.config.host_channel_id = host_channel_id
-            response_parts.append(f'Host channel updated to {host_channel_id}')
-
-        if submission_list_channel_id is not None:
-            self.bot.config.submissions_message.channel_id = submission_list_channel_id
-            response_parts.append(f'Submission message channel updated to {submission_list_channel_id}')
-
-        if response_parts:
-            self.bot.update_config()
-            await respond(interaction, '\n'.join(response_parts))
-        else:
-            await respond(interaction, 'No settings updated.')
-
-
-    @slash_command()
-    @guild_only()
-    @default_permissions()
     async def set_host_channel(self, interaction: discord.Interaction):
         """ Use this in the channel where submissions should be sent. """
         self.bot.config.host_channel_id = interaction.channel.id
